45d400da5833dd06dd44bd8e3abda29c9057f1c0
Iván Toromoreno Sarango <ivan.toromoreno@epn.edu.ec>
2020-06-19 16:55:08 -0500

    Métodos de arreglos

--- 01-javascript/02-arreglos.js
+++ 01-javascript/02-arreglos.js
@@ -3,3 +3,33 @@ const arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const arregloTodo = [
     1, 2.3, true, 'HOLA', null, undefined, {}, [1, 2, true]
 ];
+
+
+for (let numero of arreglo) { // Valores
+    console.log('numero', numero);
+}
+
+for (let indice in arreglo) { // Índices
+    console.log('numero', indice);
+}
+
+arreglo.push(11);
+console.log(arreglo);
+
+arreglo.pop();
+console.log(arreglo);
+
+arreglo.unshift(0);
+console.log(arreglo);
+
+arreglo.splice(0, 1);
+console.log(arreglo);
+
+arreglo.splice(0, 0, 1, 2, 3);
+console.log(arreglo);
+
+const indice = arreglo.indexOf(4);
+console.log(indice);
+
+arreglo.splice(indice, 1);
+console.log(arreglo);
