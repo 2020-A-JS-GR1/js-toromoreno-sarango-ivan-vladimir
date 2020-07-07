48bf702d48aa1cc316ccaf6b288bf2f693d73ce5
Iván Toromoreno Sarango <ivan.toromoreno@epn.edu.ec>
2020-07-07 17:18:29 -0500

    Final Javascript

--- 01-javascript/10-async-await.js
+++ 01-javascript/10-async-await.js
@@ -1,17 +1,38 @@
 // 10-async-await.js
 
-const promesaLeerArchivo = new Promise();
-const promesaEscribirArchivo = new Promise();
+const promesaLeerArchivo = () => {
+    return new Promise(
+        (res, rej) => {
+            // res('CONTENIDO LEER ARCHIVO')
+            res('CONTENIDO LEER ARCHIVO');
+        }
+    );
+}
+const promesaEscribirArchivo = () => {
+    return new Promise(
+        (res, rej) => {
+            // res('CONTENIDO ESCRIBIR ARCHIVO');
+            rej('ERROR =(')
+        }
+    );
+}
 
+// ESTO NO ES POSIBLE
+// PORQUE NO ESTA DENTRO DE UNA FUNCION
+// const respuesta = await promesaEscribirArchivo;
+
+// ASYNC AWAIT -> DENTRO DE UNA FUNCION
 async function ejercicio() {
     console.log('1');
     try {
         console.log('2');
         const contenidoArchivoActual = await promesaLeerArchivo();
+        console.log(contenidoArchivoActual);
         console.log('3');
         await promesaEscribirArchivo();
         console.log('4');
         const nuevoContenido = await promesaLeerArchivo();
+        console.log(nuevoContenido);
         console.log('5');
     } catch (error) {
         console.error(error);
@@ -20,6 +41,11 @@ async function ejercicio() {
     console.log('7');
 }
 
-const f = async () => {
-    // contenido
-}
+const respuestaEjercicio = ejercicio();
+//console.log('respuestaEjercicio', respuestaEjercicio);
+//console.log('promesaLeerArchivo', promesaLeerArchivo());
+//console.log('promesaEscribirArchivo', promesaEscribirArchivo());
+
+//  const f = async () => {
+// contenido
+// }
